22268c555382d1a1fb4561c56fab424bfe5256e0
Juozas Valančius <15262048+juozasvalancius@users.noreply.github.com>
2021-08-28 13:59:20 +0300

    Improve Main Screen and Row View Separation

--- Posts/MainScreen/MainScreenView.swift
+++ Posts/MainScreen/MainScreenView.swift
@@ -12,11 +12,14 @@ struct MainScreenView: View {
   var isRefreshing: Bool = false
 
   var body: some View {
-    List(viewModel.postIDs, id: \.self) { postID in
-      PostEntryView(
-        viewModel: viewModel.getRowViewModel(postID: postID),
-        navigatedPostID: $navigatedPostID
-      )
+    List(viewModel.postIDs, id: \.self, selection: $navigatedPostID) { postID in
+      NavigationLink(
+        destination: Text("Post #\(postID)"),
+        tag: postID,
+        selection: $navigatedPostID
+      ) {
+        PostEntryView(viewModel: viewModel.makeRowViewModel(postID: postID))
+      }
     }.refreshable(isRefreshing: $isRefreshing) {
       // fake refresh
       DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
--- Posts/MainScreen/PostEntryView.swift
+++ Posts/MainScreen/PostEntryView.swift
@@ -2,33 +2,26 @@ import SwiftUI
 
 struct PostEntryView: View {
 
-  let navigatedPostID: Binding<Int?>
   let viewModel: PostRowViewModel
 
-  init(viewModel: PostRowViewModel, navigatedPostID: Binding<Int?>) {
-    self.navigatedPostID = navigatedPostID
+  init(viewModel: PostRowViewModel) {
     self.viewModel = viewModel
   }
 
   var body: some View {
-    NavigationLink(
-      destination: Text("Post #\(viewModel.id)"),
-      tag: viewModel.id,
-      selection: navigatedPostID
-    ) {
-      VStack(alignment: .leading) {
-        Text(viewModel.user)
-        Text(viewModel.title)
-        Text(viewModel.body)
-      }
-      .lineLimit(1)
+    VStack(alignment: .leading) {
+      Text(viewModel.user)
+      Text(viewModel.title)
+      Text(viewModel.body)
     }
+    .lineLimit(1)
   }
+
 }
 
 struct PostEntryViewPreviews: PreviewProvider {
   static var previews: some View {
-    let viewModel = MainScreenViewModel(storage: MemoryStorage()).getRowViewModel(postID: 1)
-    PostEntryView(viewModel: viewModel, navigatedPostID: .constant(nil))
+    let viewModel = MainScreenViewModel(storage: MemoryStorage()).makeRowViewModel(postID: 1)
+    PostEntryView(viewModel: viewModel)
   }
 }
